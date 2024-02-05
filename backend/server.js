2926dcaf07d8f68df385dad5664e0d14140eacca
RIMqanvus <141220989+RIMqanvus@users.noreply.github.com>
2024-02-05 19:23:08 +0530

    greeting-card

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const app = express()
+dotenv.config()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT
+const path = require('path')
+
+if (process.env.NODE_ENV === 'local') {
+    app.use(cors({
+        origin: 'http://localhost:3000/',
+        credentials: true
+    }))
+}
+else {
+    app.use(cors({
+        credentials: true
+    }))
+}
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, './frontend/dist')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, './', 'frontend', 'dist', 'index.html'))
+    })
+}
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+})
+    .then(() => console.log("Mongodb is Connected"))
+    .catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.listen(PORT, () => 
+    console.log(`Server is running on ${PORT}`)
+  );
